03deaccea6055fcfc617d61305a7446ea410aa4a
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-12 17:28:01 -0500

    fix inputhandler

--- public/js/mobile/InputHandler.js
+++ public/js/mobile/InputHandler.js
@@ -1,5 +1,6 @@
-function InputHandler(socket){
-    
+
+function InputHandler() {
+  throw new Error('InputHandler should not be instantiated!');
 }
 
 InputHandler.PUNCH = false;
@@ -9,43 +10,41 @@ InputHandler.RIGHT = false;
 InputHandler.UP = false;
 InputHandler.DOWN = false;
 
-InputHandler.apply = function(socket){
-    $('#A').on('mousedown', function(e){
-            this.PUNCH = true;
-    });
-    $('#A').on('mouseup', function(e){
-            this.PUNCH = false;
-    });
-    $('#B').on('mousedown', function(e){
-            this.KICK = true;
-    });
-    $('#B').on('mouseup', function(e){
-            this.KICK = false;
-    });
-    $('#left').on('mousedown', function(e){
-            this.LEFT = true;
-    });
-    $('#left').on('mouseup', function(e){
-            this.LEFT = false;
-    });
-    $('#right').on('mousedown', function(e){
-            this.RIGHT = true;
-    });
-    $('#right').on('mouseup', function(e){
-            this.RIGHT = false;
-    });
-    $('#up').on('mousedown', function(e){
-            this.UP = true;
-    });
-    $('#up').on('mouseup', function(e){
-            this.UP = false;
-    });
-    $('#down').on('mousedown', function(e){
-            this.DOWN = true;
-    });
-    $('#down').on('mouseup', function(e){
-            this.DOWN = false;
-    });
-    
-    
-}
+InputHandler.apply = function(){
+  $('#A').on('mousedown', function(e) {
+    InputHandler.PUNCH = true;
+  });
+  $('#A').on('mouseup', function(e) {
+    InputHandler.PUNCH = false;
+  });
+  $('#B').on('mousedown', function(e) {
+    InputHandler.KICK = true;
+  });
+  $('#B').on('mouseup', function(e) {
+    InputHandler.KICK = false;
+  });
+  $('#left').on('mousedown', function(e) {
+    InputHandler.LEFT = true;
+  });
+  $('#left').on('mouseup', function(e) {
+    InputHandler.LEFT = false;
+  });
+  $('#right').on('mousedown', function(e) {
+    InputHandler.RIGHT = true;
+  });
+  $('#right').on('mouseup', function(e) {
+    InputHandler.RIGHT = false;
+  });
+  $('#up').on('mousedown', function(e) {
+    InputHandler.UP = true;
+  });
+  $('#up').on('mouseup', function(e) {
+    InputHandler.UP = false;
+  });
+  $('#down').on('mousedown', function(e) {
+    InputHandler.DOWN = true;
+  });
+  $('#down').on('mouseup', function(e) {
+    InputHandler.DOWN = false;
+  });
+};
