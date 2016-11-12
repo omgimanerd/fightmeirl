ae804fc803182b591a2b6d1161bff1c4d23fea4f
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-12 16:16:22 -0500

    pairing logic and player instantiation

--- shared/Constants.js
+++ shared/Constants.js
@@ -15,6 +15,10 @@ Constants.CANVAS_WIDTH = 800;
 
 Constants.CANVAS_HEIGHT = 600;
 
+Constants.WORLD_MIN = 0;
+
+Constants.WORLD_MAX = 2500;
+
 if (typeof module === 'object') {
   /**
    * If Constants is loaded as a Node module, then this line is called.
