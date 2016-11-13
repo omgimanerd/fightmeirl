0f7f35fc1704a7eb646c4529a9feac8133d74d35
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-13 12:05:52 -0500

    power level

--- public/js/desktop/Game.js
+++ public/js/desktop/Game.js
@@ -112,12 +112,11 @@ Game.prototype.draw = function() {
       player['width'], player['height'], player['health'],
       player['isPunching'], player['isKicking']
     );
-    console.log(player['screamPower']);
     $('.scream-volume').width((player['screamPower'] - 1) * 100);
-    if (player['screamPower'] > 4.5) {
+    if (player['screamPower'] > 4) {
       $('.scream-volume').text('IT\'S OVER 9000');
     } else {
-      $('.scream-volume').text(player['screamPower'] * 2000);
+      $('.scream-volume').text((player['screamPower'] - 1) * 3000);
     }
 
     // Iterate through players, draw each using info from json obj.
