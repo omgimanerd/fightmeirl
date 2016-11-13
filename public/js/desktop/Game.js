fde345367e89951c0147237aa0704dd0a26386d4
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-13 11:50:34 -0500

    better scream behavior

--- public/js/desktop/Game.js
+++ public/js/desktop/Game.js
@@ -113,7 +113,7 @@ Game.prototype.draw = function() {
       player['isPunching'], player['isKicking']
     );
     console.log(player['screamPower']);
-    $('.scream-volume').width((player['screamPower'] - 1) * 25);
+    $('.scream-volume').width((player['screamPower'] - 1) * 30);
 
     // Iterate through players, draw each using info from json obj.
     for (var i = 0; i < this.players.length; ++i) {
